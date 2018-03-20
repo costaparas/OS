6eb56f5d916d3ca4d820afb8927d05920c81724f
Dominic Fung <domokun997@gmail.com>
2018-03-20 13:46:19 +1100

    Implement producer consumer

--- asst1-src/kern/asst1/producerconsumer.c
+++ asst1-src/kern/asst1/producerconsumer.c
@@ -1,13 +1,23 @@
 /* This file will contain your solution. Modify it as you wish. */
 #include <types.h>
+#include <synch.h>
+#include <lib.h>
 #include "producerconsumer_driver.h"
 
 /* Declare any variables you need here to keep track of and
    synchronise your bounded. A sample declaration of a buffer is shown
    below. You can change this if you choose another implementation. */
 
-static struct pc_data buffer[BUFFER_SIZE];
+struct _buffer {
+    struct pc_data data[BUFFER_SIZE];
+    uint32_t head, tail; // tail is next available slot
+};
 
+static struct _buffer buffer;
+
+static struct cv *has_capacity;
+static struct cv *has_data;
+static struct lock *buffer_lock;
 
 /* consumer_receive() is called by a consumer to request more data. It
    should block on a sync primitive if no data is available in your
@@ -15,13 +25,15 @@ static struct pc_data buffer[BUFFER_SIZE];
 
 struct pc_data consumer_receive(void)
 {
-        struct pc_data thedata;
+	lock_acquire(buffer_lock);
+	while ((buffer.tail - buffer.head) % BUFFER_SIZE == 0)
+		cv_wait(has_data, buffer_lock);
 
-        (void) buffer; /* remove this line when you start */
+        struct pc_data thedata = buffer.data[buffer.head++];
+	buffer.head %= BUFFER_SIZE;
 
-        /* FIXME: this data should come from your buffer, obviously... */
-        thedata.item1 = 1;
-        thedata.item2 = 2;
+	cv_signal(has_capacity, buffer_lock);
+	lock_release(buffer_lock);
 
         return thedata;
 }
@@ -31,7 +43,15 @@ struct pc_data consumer_receive(void)
 
 void producer_send(struct pc_data item)
 {
-        (void) item; /* Remove this when you add your code */
+	lock_acquire(buffer_lock);
+	while ((buffer.tail - buffer.head) % BUFFER_SIZE == BUFFER_SIZE)
+		cv_wait(has_capacity, buffer_lock);
+
+	buffer.data[buffer.tail++] = item;
+	buffer.tail %= BUFFER_SIZE;
+
+	cv_signal(has_data, buffer_lock);
+	lock_release(buffer_lock);
 }
 
 
@@ -42,10 +62,27 @@ void producer_send(struct pc_data item)
 
 void producerconsumer_startup(void)
 {
+        has_capacity = cv_create("has capacity");
+        has_data = cv_create("has data");
+        buffer_lock = lock_create("buffer lock");
+
+	if (has_capacity == NULL)
+		panic("%s: has_capacity create failed", __FILE__);
+	if (has_data == NULL)
+		panic("%s: has_data create failed", __FILE__);
+	if (buffer_lock == NULL)
+		panic("%s: buffer_lock create failed", __FILE__);
+
+	// Initialise buffer props
+	buffer.head = 0;
+        buffer.tail = 0;
 }
 
 /* Perform any clean-up you need here */
 void producerconsumer_shutdown(void)
 {
+        cv_destroy(has_capacity);
+        cv_destroy(has_data);
+        lock_destroy(buffer_lock);
 }
 
