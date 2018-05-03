e4eacbf5066740f0c8ad64dcb3ff336ae51ec8bc
Dominic Fung <domokun997@gmail.com>
2018-05-04 01:17:19 +1000

    Add some assertions for write (printfs are still there)

--- asst2-src/userland/testbin/asst2/test_write.c
+++ asst2-src/userland/testbin/asst2/test_write.c
@@ -8,6 +8,7 @@ void test_write(void) {
 	if (fd >= 0) {
 		char buf[] = "hello world\n";
 		int bytes = write(fd, buf, 12);
+		assert(bytes == 12);
 		printf("bytes written - should be 12: %d\n", bytes);
 		printf("check buffer is still in tact - "
 		       "should be 'hello world\\n': '%s'\n", buf);
@@ -20,6 +21,7 @@ void test_write(void) {
 		char buf2[51];
 		bytes = read(fd, buf2, 49);
 		buf2[bytes] = '\0';
+		assert(bytes == 12);
 		printf("bytes read - should be 12: %d\n", bytes);
 		printf("check if buffer is correct - "
 		       "should be 'hello world\\n': '%s'\n", buf2);
@@ -47,6 +49,7 @@ void test_write(void) {
 	if (fd >= 0) {
 		char buf[] = "this is a test\n";
 		int bytes = write(fd, buf, 15);
+		assert(bytes == 15);
 		printf("bytes written - should be 15: %d\n", bytes);
 		printf("check buffer is still in tact - "
 		       "should be 'this is a test': '%s'\n", buf);
@@ -59,6 +62,7 @@ void test_write(void) {
 		char buf2[51];
 		bytes = read(fd, buf2, 49);
 		buf2[bytes] = '\0';
+		assert(bytes == 15);
 		printf("bytes read - should be 15: %d\n", bytes);
 		printf("check if buffer is correct - "
 		       "should be 'this is a test\\n': '%s'\n", buf2);
@@ -74,6 +78,7 @@ void test_write(void) {
 	if (fd >= 0) {
 		char buf[] = "blablabla\n";
 		int bytes = write(fd, buf, 10);
+		assert(bytes == 10);
 		printf("bytes written - should be 10: %d\n", bytes);
 		printf("check buffer is still in tact - "
 		       "should be 'blablabla\\n': '%s'\n", buf);
@@ -86,6 +91,7 @@ void test_write(void) {
 		char buf2[51];
 		bytes = read(fd, buf2, 49);
 		buf2[bytes] = '\0';
+		assert(bytes == 10);
 		printf("bytes read - should be 10: %d\n", bytes);
 		printf("check if buffer is correct - "
 		       "should be 'blablabla\\n': '%s'\n", buf2);
@@ -101,6 +107,7 @@ void test_write(void) {
 	if (fd >= 0) {
 		char buf[] = "should be appended\n";
 		int bytes = write(fd, buf, 19);
+		assert(bytes == 19);
 		printf("bytes written - should be 19: %d\n", bytes);
 		printf("check buffer is still in tact - "
 		       "should be 'should be appended\\n': '%s'\n", buf);
@@ -113,6 +120,7 @@ void test_write(void) {
 		char buf2[51];
 		bytes = read(fd, buf2, 49);
 		buf2[bytes] = '\0';
+		assert(bytes == 29);
 		printf("bytes read - should be 29: %d\n", bytes);
 		printf("check if buffer is correct - "
 		       "should be 'blablabla\\nshould be appended\\n': '%s'\n", buf2);
@@ -136,6 +144,7 @@ void test_write(void) {
 		char buf[51];
 		int bytes = read(fd, buf, 49);
 		buf[bytes] = '\0';
+		assert(bytes == 29);
 		printf("bytes read - should be 29: %d\n", bytes);
 		printf("check if buffer is correct - "
 		       "should be 'blablabla\\nshould be appended\\n': '%s'\n", buf);
@@ -151,6 +160,7 @@ void test_write(void) {
 	if (fd >= 0) {
 		char buf[101];
 		int bytes = write(fd, buf, 0);
+		assert(bytes == 0);
 		printf("bytes written - should be 0: %d\n", bytes);
 		int r = close(fd);
 		if (r) printf("error (should not print!): %s\n\n", strerror(errno));
@@ -160,6 +170,7 @@ void test_write(void) {
 		char buf2[51];
 		bytes = read(fd, buf2, 49);
 		buf2[bytes] = '\0';
+		assert(bytes == 29);
 		printf("bytes read - should be 29: %d\n", bytes);
 		printf("check if buffer is correct - "
 		       "should be 'blablabla\\nshould be appended\\n': '%s'\n", buf2);
@@ -175,9 +186,11 @@ void test_write(void) {
 	if (fd >= 0) {
 		char buf[] = "this new text will overwrite\nthe previous file\n";
 		int bytes = write(fd, buf, 47);
+		assert(bytes == 47);
 		printf("bytes written - should be 47: %d\n", bytes);
 		char buf2[101];
 		bytes = read(fd, buf2, 99);
+		assert(bytes == 0);
 		printf("bytes read - should be 0: %d\n", bytes);
 		int r = close(fd);
 		if (r) printf("error (should not print!): %s\n\n", strerror(errno));
