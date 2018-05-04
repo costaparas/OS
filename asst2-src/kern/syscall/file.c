ae1bc370f632e891d08d197d74634161291282e8
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-05-04 13:21:42 +1000

    remove all kprintfs from file.c

--- asst2-src/kern/syscall/file.c
+++ asst2-src/kern/syscall/file.c
@@ -24,7 +24,6 @@ struct OF **open_files = NULL;
  * Called from boot(), so no synchronization is required.
  */
 void fs_bootstrap() {
-	kprintf("INIT FS...\n"); /* TODO: debug-only */
 	num_files = 0;
 	open_files = NULL;
 	of_lock = lock_create("open file table lock");
@@ -36,7 +35,6 @@ void fs_bootstrap() {
  * Called from shutdown(), so no synchronization is required.
  */
 void fs_clear_tables() {
-	kprintf("FREEING FS...\n"); /* TODO: debug-only */
 	for (uint32_t i = 0; i < num_files; i++) {
 		if (open_files[i]->file_lock != NULL) {
 			lock_destroy(open_files[i]->file_lock);
@@ -64,7 +62,6 @@ bool valid_fd(uint32_t fd) {
  * Initialise the fd table for the process.
  */
 int init_fd_table() {
-	kprintf("INIT FD TABLE...\n"); /* TODO: debug-only */
 	struct FD **fds = kmalloc(sizeof(struct FD *) * OPEN_MAX);
 	if (fds == NULL) return ENOMEM;
 	curproc->fds = fds;
@@ -88,8 +85,6 @@ int sys_open(const_userptr_t path, uint32_t flags, mode_t mode, int *fd) {
 	size_t path_kern_size = 0;
 	int ret = copyinstr(path, path_kern, (size_t) PATH_MAX, &path_kern_size);
 	if (ret) return ret; /* rest of error-checking handled here */
-
-	kprintf("OPENING FILE...%s %d\n", path_kern, flags); /* TODO: debug-only */
 	struct FD **fds = curproc->fds;
 
 	/* look for the first available fd */
@@ -100,24 +95,17 @@ int sys_open(const_userptr_t path, uint32_t flags, mode_t mode, int *fd) {
 			break;
 		}
 	}
-	if (fd_found == -1) {
-		kprintf("Ran out of file descriptors\n"); /* TODO: debug-only */
-		return EMFILE; /* reached max open files for this process */
-	}
+	if (fd_found == -1) return EMFILE; /* reached max open files for this process */
 
 	/* set can_read/can_write flags according to flags argument */
 	fds[fd_found]->can_read = true;
 	fds[fd_found]->can_write = true;
 	if ((flags & O_RDWR) != 0) {
-		kprintf("CAN READ AND WRITE\n"); /* TODO: debug-only */
 	} else if ((flags & O_WRONLY) != 0) {
-		kprintf("CAN WRITE ONLY\n"); /* TODO: debug-only */
 		fds[fd_found]->can_read = false;
 	} else if (flags == O_RDONLY) {
-		kprintf("CAN READ ONLY\n"); /* TODO: debug-only */
 		fds[fd_found]->can_write = false;
 	} else {
-		kprintf("Invalid flags to open %d\n", flags); /* TODO: debug-only */
 		return EINVAL; /* unknown flags */
 	}
 
@@ -125,10 +113,7 @@ int sys_open(const_userptr_t path, uint32_t flags, mode_t mode, int *fd) {
 	struct vnode vn;
 	struct vnode *v = &vn;
 	ret = vfs_open(path_kern, flags, mode, &v);
-	if (ret) {
-		kprintf("Error being returned from vfs_open %d\n", ret); /* TODO: debug-only */
-		return ret; /* rest of error-checking handled here */
-	}
+	if (ret) return ret; /* rest of error-checking handled here */
 
 	/* create a new entry in open file table for the vnode */
 	lock_acquire(of_lock);
@@ -156,7 +141,6 @@ int sys_open(const_userptr_t path, uint32_t flags, mode_t mode, int *fd) {
 
 	fds[fd_found]->free = false; /* mark this fd as used */
 	*fd = fd_found;
-	kprintf("fd returned: %d\n", fd_found); /* TODO: debug-only */
 	return 0;
 }
 
@@ -164,7 +148,6 @@ int sys_open(const_userptr_t path, uint32_t flags, mode_t mode, int *fd) {
  * Close an open file. Return EBADF for an invalid fd.
  */
 int sys_close(uint32_t fd) {
-	kprintf("CLOSING FILE...%d\n\n", fd); /* TODO: debug-only */
 	struct FD **fds = curproc->fds;
 	if (!valid_fd(fd)) return EBADF;
 
@@ -193,7 +176,6 @@ int sys_close(uint32_t fd) {
  * Read up to buflen bytes into the buffer buf and return number of bytes read.
  */
 int sys_read(uint32_t fd, const_userptr_t buf, size_t buflen, size_t *read) {
-	kprintf("READING FILE...%d %d\n", fd, buflen); /* TODO: debug-only */
 	struct FD **fds = curproc->fds;
 	if (!valid_fd(fd)) {
 		return EBADF;
@@ -226,12 +208,6 @@ int sys_read(uint32_t fd, const_userptr_t buf, size_t buflen, size_t *read) {
 	lock_acquire(fds[fd]->file->file_lock);
 	fds[fd]->file->offset += resid - u.uio_resid;
 	lock_release(fds[fd]->file->file_lock);
-
-	/* TODO: debug-only */
-	/*kprintf("(kernel) BUF CONTENTS:\n");
-	kprintf("###################\n");
-	kprintf("%s", (char *) buf_kern);
-	kprintf("###################\n");*/
 
 	*read = resid - u.uio_resid;
 	return 0;
@@ -284,7 +260,6 @@ int sys_write(uint32_t fd, const_userptr_t buf, size_t nbytes, size_t *written) 
  * Change the file pointer offset of an open file.
  */
 int sys_lseek(uint32_t fd, off_t pos, int whence, off_t *ret) {
-	kprintf("SEEKING FILE...%d %lld %d\n", fd, pos, whence); /* TODO: debug-only */
 	struct FD **fds = curproc->fds;
 	if (!valid_fd(fd)) return EBADF;
 
@@ -326,8 +301,6 @@ int sys_lseek(uint32_t fd, off_t pos, int whence, off_t *ret) {
  * Clone file handles.
  */
 int sys_dup2(int32_t oldfd, int32_t newfd, int32_t *retfd) {
-	kprintf("(kern) DUPLICATING FD...old:%d new:%d\n", oldfd, newfd); /* TODO: debug-only */
-	kprintf("(kern) OLD: %d, NEW: %d\n", oldfd, newfd); /* TODO: debug-only */
 	if (!valid_fd(oldfd) || newfd < 0 || newfd >= OPEN_MAX) return EBADF;
 
 	/* do nothing if both FDs are identical */
